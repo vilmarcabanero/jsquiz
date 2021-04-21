1a2775afdda22ec86b9a21b29dec9fa220842274
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-21 23:09:38 +0800

    Added showResults()

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const buildQuiz = () => {
 	// for each question...
 	myQuestions.forEach((currentQuestion, questionNumber) => {
 		// variable to store the list of possible answers
-    // we'll want to store the list of answer choices
+		// we'll want to store the list of answer choices
 		const answers = []
 		// and for each available answer...
 		for (letter in currentQuestion.answers) {
@@ -51,7 +51,7 @@ const buildQuiz = () => {
             <input type="radio" name="question${questionNumber}" value="${letter}">
             ${letter} :
             ${currentQuestion.answers[letter]}
-          </label> <br/>`
+          </label>`
 			)
 		}
 
@@ -67,7 +67,38 @@ const buildQuiz = () => {
 	quizContainer.innerHTML = output.join('')
 }
 
-const showResults = () => {}
+const showResults = () => {
+	// gather answer containers from our quiz
+	const answerContainers = quizContainer.querySelectorAll('.answers')
+
+	// keep track of user's answers
+	let numCorrect = 0
+
+	// for each question...
+	myQuestions.forEach((currentQuestion, questionNumber) => {
+		// find selected answer
+		const answerContainer = answerContainers[questionNumber]
+		const selector = `input[name=question${questionNumber}]:checked`
+		const userAnswer = (answerContainer.querySelector(selector) || {}).value
+
+		// if answer is correct
+		if (userAnswer === currentQuestion.correctAnswer) {
+			// add to the number of correct answers
+			numCorrect++
+
+			// color the answers green
+			answerContainers[questionNumber].style.color = 'lightgreen'
+		}
+		// if answer is wrong or blank
+		else {
+			// color the answers red
+			answerContainers[questionNumber].style.color = 'red'
+		}
+	})
+
+	// show number of correct answers out of total
+	resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`
+}
 
 buildQuiz()
 
